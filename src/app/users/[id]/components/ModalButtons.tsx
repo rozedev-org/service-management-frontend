9e6363964a7f4e7653886f958c21bc101ab74c76
David Ibarra <ibarrarojashector@gmail.com>
2024-03-19 15:39:07 -0400

    first commit

---
+++ src/app/users/[id]/components/ModalButtons.tsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from '@chakra-ui/react'
+import axios from 'axios'
+import router from 'next/router'
+import { useUser } from '../../hook/useUser'
+
+export default function ModalButtons({ params }: { params: { id: number } }) {
+  const userQuery = useUser(params.id)
+  const { onOpen, isOpen, onClose } = useDisclosure()
+  const handleDelete = async () => {
+    await axios.delete(
+      `http://localhost:8000/api/service-manager-service/v1/users/${params.id}`
+    )
+    router.push('/users')
+  }
+
+  return (
+    <>
+      <Button colorScheme='red' margin='10px' onClick={onOpen}>
+        Eliminar
+      </Button>
+      <Button colorScheme='blue' margin='10px'>
+        Actualizar
+      </Button>
+      <Modal
+        isCentered
+        onClose={onClose}
+        isOpen={isOpen}
+        motionPreset='slideInBottom'
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            Eliminacion del Usuario : {userQuery.data?.userName}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Confirmacion de Eliminacion</ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Cerrar
+            </Button>
+            <Button colorScheme='red' onClick={handleDelete}>
+              Confirmar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,3 +1,35 @@
+'use client'
+import {
+  Text,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  Avatar,
+} from '@chakra-ui/react'
+import { useUser } from '../hook/useUser'
+import ModalButtons from './components/ModalButtons'
+
 export default function UserPage({ params }: { params: { id: number } }) {
-  return <main> este es el User page. user {params.id}</main>
+  const userQuery = useUser(params.id)
+  return (
+    <main style={{ alignItems: 'center' }}>
+      <Card>
+        <CardHeader display='flex' flexDirection='column' alignItems='center'>
+          <Avatar name={userQuery.data?.userName} size={'sm'} p='7' />
+          <Heading size='md'>Usuario : {userQuery.data?.userName}</Heading>
+          <Text>ID del Usuario : {userQuery.data?.id}</Text>
+        </CardHeader>
+        <CardBody>
+          <Text>Nombre y Apellido:</Text>
+          <Text>{userQuery.data?.firstName}</Text>
+          <Text>{userQuery.data?.lastName}</Text>
+        </CardBody>
+        <CardFooter>
+          <ModalButtons params={params} />
+        </CardFooter>
+      </Card>
+    </main>
+  )
 }
