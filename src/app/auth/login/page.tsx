ad887db4c2dbb54757d68aa91758ab64a4562a32
David Ibarra <ibarrarojashector@gmail.com>
2024-05-07 09:59:00 -0400

    validacion de rutas al hacer login y logout

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 import { CardContainer } from '@/components/Card/CardContainer/CardContainer'
 import React, { useEffect, useState } from 'react'
 import { LoginForm } from './components/LoginForm'
+import { useUserSession } from '@/states/useUserId'
+import { useRouter } from 'next/navigation'
+import { appRoutes } from '@/appRoutes'
 
 export default function LoginPage() {
   const [isLoading, setIsLoading] = useState<boolean>(true)
-
+  const { isLoggedIn } = useUserSession()
+  const router = useRouter()
   useEffect(() => {
+    if (isLoggedIn) {
+      router.push(appRoutes.home.url(0))
+    }
     setIsLoading(false)
-  }, [])
+  }, [isLoggedIn])
 
   return (
     <CardContainer title='Login' isLoading={isLoading}>
